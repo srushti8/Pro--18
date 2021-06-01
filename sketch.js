abd5d0ddca4c28a2f2333da3471da4f8b14ae57e
srushti8 <76839929+srushti8@users.noreply.github.com>
2021-06-01 15:48:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var roof, ground;
+var bob1, bob2, bob3, bob4, bob5;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	roof = new Roof(400,200,600,40);
+	ground = new Roof(400,700,800,10);
+	bob1 = new Bob(200,300,100);
+	bob2 = new Bob(300,300,100);
+	bob3 = new Bob(400,300,100);
+	bob4 = new Bob(500,300,100);
+	bob5 = new Bob(600,300,100);
+
+	rope1 = new Rope(bob1.body,roof.body,-100,0);
+	//World.add(world, rope1)
+
+	Engine.run(engine);
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  roof.display();
+  ground.display();
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+  rope1.display();
+
+
+  drawSprites();
+}
+
+
